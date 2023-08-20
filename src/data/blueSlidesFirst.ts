d9feb04d4dbd58f463d724dc3073342df815dbec
Mulsi <mulej.miha0@gmail.com>
2023-08-20 11:03:35 +0200

    Attepmt to solve the ios issue with set timeout

--- src/data/blueSlidesFirst.ts
+++ src/data/blueSlidesFirst.ts
@@ -12,18 +12,6 @@ import blue19 from '../../public/images/BLUE/_O1I9384.jpeg'
 import blue22 from '../../public/images/BLUE/binelighttrailmodra2.jpg'
 import blue23 from '../../public/images/BLUE/DJI_0071.jpeg'
 import blue29 from '../../public/images/BLUE/R11.jpg'
-import blue10 from '../../public/images/BLUE/_C6B7078.jpg'
-import blue12 from '../../public/images/BLUE/_C6B8971.jpg'
-import blue14 from '../../public/images/BLUE/micvajzresized.jpg'
-import blue15 from '../../public/images/BLUE/_K4I3871-2.jpeg'
-import blue16 from '../../public/images/BLUE/_K4I7188.jpg'
-import blue18 from '../../public/images/BLUE/_O1I1323.jpg'
-import blue20 from '../../public/images/BLUE/_O1I9550.jpeg'
-import blue21 from '../../public/images/BLUE/_V3A0084.jpg'
-import blue25 from '../../public/images/BLUE/Najvogelhanddrag.jpg'
-import blue26 from '../../public/images/BLUE/NF_20220816_Skydiving_over_Landshut_7842.jpeg'
-import blue27 from '../../public/images/BLUE/NF_20220817_Skydiving_over_Landshut_8378.jpeg'
-import blue28 from '../../public/images/BLUE/NF_20220818_Skydiving_over_Landshut_8722.jpeg'
 import { photo } from '@/photo'
 
 const blueSection: photo[] = [
@@ -110,78 +98,6 @@ const blueSection: photo[] = [
         width: 3585,
         height: 1976,
         alt: 'Surfing',
-    },
-    {
-        src: blue14.src,
-        width: 5028,
-        height: 3352,
-        alt: 'Backcountry snowboarding',        
-    },
-    {
-        src: blue15.src,
-        width: 2210,
-        height: 1381,
-        alt: 'Snowboarder',        
-    },
-    {
-        src: blue18.src,
-        width: 5257,
-        height: 3505,
-        alt: 'Snowbarder',        
-    },
-    {
-        src: blue20.src,
-        width: 5461,
-        height: 3616,
-        alt: 'Fog',        
-    },
-    {
-        src: blue21.src,
-        width: 5199,
-        height: 3249,
-        alt: 'Ski touring',        
-    },
-    {
-        src: blue27.src,
-        width: 2289,
-        height: 3433,
-        alt: 'Redbull',        
-    },
-    {
-        src: blue28.src,
-        width: 4624,
-        height: 3083,
-        alt: 'Redbull',        
-    },
-    {
-        src: blue26.src,
-        width: 4506,
-        height: 3004,
-        alt: 'Athlete',        
-    },
-    {
-        src: blue25.src,
-        width: 4088,
-        height: 2555,
-        alt: 'Bike',        
-    },
-    {
-        src: blue10.src,
-        width: 3590,
-        height: 4794,
-        alt: 'Above the clouds',        
-    },
-    {
-        src: blue16.src,
-        width: 4443,
-        height: 2954,
-        alt: 'BMX',        
-    },
-    {
-        src: blue12.src,
-        width: 2486,
-        height: 2464,
-        alt: 'Surfing',        
     },
 ];
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,19 +6,22 @@ import { useState } from 'react'
 // import { useCallCount } from '../contexts/CallCountContext';
 import PhotoAlbum, { RenderPhotoProps } from "react-photo-album";
 import Image from "next/image";
-import blueSlides from '@/data/blueSlides'
+import blueSlidesFirst from '@/data/blueSlidesFirst'
+import blueSlidesSecond from '@/data/blueSlidesSecond'
 import greenSlides from '@/data/greenSlides';
 import pastelSlides from '@/data/pastelSlides';
 import whiteSlides from '@/data/whiteSlides';
 import bnwSlides from '@/data/BnwSlides';
 
 export default function Home() {
-  const [indexBlue, setIndexBlue] = useState(-1);
+  const [indexBlueFirst, setIndexBlueFirst] = useState(-1);
+  const [indexBlueSecond, setIndexBlueSecond] = useState(-1);
   const [indexGreen, setIndexGreen] = useState(-1);
   const [indexPastel, setIndexPastel] = useState(-1);
   const [indexWhite, setIndexWhite] = useState(-1);
   const [indexBnw, setIndexBnw] = useState(-1);
   // const { callCount, setCallCount } = useCallCount();
+
 
   // Load callCount from local storage on initial render
   // useEffect(() => {
@@ -33,7 +36,16 @@ export default function Home() {
   //   sessionStorage.setItem('callCount', callCount.toString());
   // }, [callCount]);
 
+  // function incrementCallCount() {
+  //   if (callCount < 76) { 
+  //     const increment = callCount + 1;
+  //     setCallCount(increment);
+  //   }
+  // }
+
   // function setNextSectionVisible() {
+  //   count = count + 1
+  //   console.log(count)
   //   setCallCount(callCount + 1);
   // }
 
@@ -53,36 +65,59 @@ export default function Home() {
       <main className='w-11/12 mx-auto'>
         <div className='blue-section-first'>
           <div>
-            <PhotoAlbum layout='masonry' photos={blueSlides} renderPhoto={renderNextJsImage} columns={(containerWidth) => {
+            <PhotoAlbum layout='masonry' photos={blueSlidesFirst} renderPhoto={renderNextJsImage} columns={(containerWidth) => {
               if (containerWidth < 550) return 2;
               return 3;
-            }} onClick={({ index: current }) => setIndexBlue(current)} />
+            }} onClick={({ index: current }) => setIndexBlueFirst(current)} />
             <Lightbox
-              index={indexBlue}
-              slides={blueSlides}
-              open={indexBlue >= 0}
-              close={() => setIndexBlue(-1)}
+              index={indexBlueFirst}
+              slides={blueSlidesFirst}
+              open={indexBlueFirst >= 0}
+              close={() => setIndexBlueFirst(-1)}
             />
 
           </div>
         </div>
 
-          <div className='green-section'>
-            <div className='mt-[15px]'>
-              <PhotoAlbum layout='masonry' photos={greenSlides} renderPhoto={renderNextJsImage} columns={(containerWidth) => {
+        {setTimeout((() => { }), 400) &&
+          <div className='blue-section-second'>
+            <div>
+              <PhotoAlbum layout='masonry' photos={blueSlidesSecond} renderPhoto={renderNextJsImage} columns={(containerWidth) => {
                 if (containerWidth < 550) return 2;
                 return 3;
-              }} onClick={({ index: current }) => setIndexGreen(current)} />
+              }} onClick={({ index: current }) => setIndexBlueSecond(current)} />
               <Lightbox
-                index={indexGreen}
-                slides={greenSlides}
-                open={indexGreen >= 0}
-                close={() => setIndexGreen(-1)}
+                index={indexBlueSecond}
+                slides={blueSlidesSecond}
+                open={indexBlueSecond >= 0}
+                close={() => setIndexBlueSecond(-1)}
               />
+
             </div>
           </div>
+        }
 
-        <div className='pastel-section'>
+
+        {setTimeout((() => { }), 800) && <div className='green-section'>
+          <div className='mt-[15px]'>
+            <PhotoAlbum layout='masonry' photos={greenSlides} renderPhoto={renderNextJsImage} columns={(containerWidth) => {
+              if (containerWidth < 550) return 2;
+              return 3;
+            }} onClick={({ index: current }) => setIndexGreen(current)} />
+            <Lightbox
+              index={indexGreen}
+              slides={greenSlides}
+              open={indexGreen >= 0}
+              close={() => setIndexGreen(-1)}
+            />
+
+          </div>
+        </div>
+        }
+
+
+
+        {setTimeout((() => { }), 1200) && <div className='pastel-section'>
           <div className='mt-[15px]'>
             <PhotoAlbum layout='masonry' photos={pastelSlides} renderPhoto={renderNextJsImage} columns={(containerWidth) => {
               if (containerWidth < 550) return 2;
@@ -94,10 +129,12 @@ export default function Home() {
               open={indexPastel >= 0}
               close={() => setIndexPastel(-1)}
             />
+
           </div>
         </div>
-        
-        <div className='white-section'>
+        }
+
+        {setTimeout((() => { }), 1400) && <div className='white-section'>
           <div className='mt-[15px]'>
             <PhotoAlbum layout='masonry' photos={whiteSlides} renderPhoto={renderNextJsImage} columns={(containerWidth) => {
               if (containerWidth < 550) return 2;
@@ -109,10 +146,12 @@ export default function Home() {
               open={indexWhite >= 0}
               close={() => setIndexWhite(-1)}
             />
+
           </div>
         </div>
+        }
 
-        <div className='bnw-section'>
+        {setTimeout((() => { }), 1500) && <div className='bnw-section'>
           <div className='mt-[15px]'>
             <PhotoAlbum layout='masonry' photos={bnwSlides} renderPhoto={renderNextJsImage} columns={(containerWidth) => {
               if (containerWidth < 550) return 2;
@@ -124,8 +163,11 @@ export default function Home() {
               open={indexBnw >= 0}
               close={() => setIndexBnw(-1)}
             />
+
           </div>
         </div>
+        }
+
       </main>
       <Footer></Footer>
     </>
