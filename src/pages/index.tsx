58bf472c9a2b9956b4dfce744baaaf725ce09d03
Mulsi <mulej.miha0@gmail.com>
2023-09-24 22:04:23 +0200

    Added mobile scroll logic for show more cta button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,8 +20,12 @@ export default function Home() {
   const [loading, setLoading] = useState(true)
   const [storedData, setStoredData] = useState(null);
   const { t } = useTranslation();
+  const [innerWidth, setInnerWidth] = useState(0);
+  const [showMobileCta, setShowMobileCta] = useState(false);
+  let timer: any = null;
 
   useEffect(() => {
+    setInnerWidth(window.innerWidth);
     const storedItem: any = localStorage.getItem('loaderShown');
     if (storedItem) {
       setStoredData(storedItem);
@@ -34,6 +38,27 @@ export default function Home() {
     }), 2500)
     return () => clearTimeout(timer)
   })
+
+  const handleMobileScroll = () => {
+    if (window.innerWidth < 640) {
+      setShowMobileCta(true);
+
+      if (timer !== null) {
+        clearTimeout(timer);
+      }
+      timer = setTimeout(function () {
+        setShowMobileCta(false);
+      }, 150);
+
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleMobileScroll);
+    return () => {
+      window.removeEventListener("scroll", handleMobileScroll);
+    };
+  }, [])
 
   const goToBlueDetail = () => {
     window.location.href = '/blue';
@@ -60,24 +85,24 @@ export default function Home() {
         <div className={`flex flex-col gap-12 justify-center mb-8`}>
           <div className='blue relative' onMouseEnter={() => setShowBlueButton(true)} onMouseLeave={() => setShowBlueButton(false)}>
             <Image src={mainBlue} alt="Mountains" priority />
-            <button style={{ opacity: showBlueButton ? 1 : 0 }} className='button-animation absolute top-1/2 left-1/2 -translate-y-2/4 -translate-x-2/4 text-xs w-[125px] h-[125px] border-2 border-white rounded-full' onClick={goToBlueDetail}>{t('showMore')}</button>
+            <button style={{ opacity: showBlueButton ? 1 : showMobileCta ? 1 : 0 }} className='button-animation absolute top-1/2 left-1/2 -translate-y-2/4 -translate-x-2/4 text-xs w-[125px] h-[125px] border-2 border-white rounded-full' onClick={goToBlueDetail}>{t('showMore')}</button>
             {/* <Link href='/blue' style={{ opacity: showBlueButton ? 1 : 0 }} className='button-animation flex items-center justify-center absolute top-1/2 left-1/2 -translate-y-2/4 -translate-x-2/4 text-xs w-[125px] h-[125px] border-2 border-white rounded-full'>{t('showMore')}</Link> */}
           </div>
           <div className='green relative' onMouseEnter={() => setShowGreenButton(true)} onMouseLeave={() => setShowGreenButton(false)}>
             <Image src={mainGreen} alt="Bine Žalohar" priority />
-            <button style={{ opacity: showGreenButton ? 1 : 0 }} className='button-animation absolute top-1/2 left-1/2 -translate-y-2/4 -translate-x-2/4 text-xs w-[125px] h-[125px] border-2 border-white rounded-full' onClick={goToGreenDetail}>{t('showMore')}</button>
+            <button style={{ opacity: showGreenButton ? 1 : showMobileCta ? 1 : 0 }} className='button-animation absolute top-1/2 left-1/2 -translate-y-2/4 -translate-x-2/4 text-xs w-[125px] h-[125px] border-2 border-white rounded-full' onClick={goToGreenDetail}>{t('showMore')}</button>
           </div>
           <div className='pastel relative' onMouseEnter={() => setShowPastelButton(true)} onMouseLeave={() => setShowPastelButton(false)}>
             <Image src={mainPastel} alt="Wallrider" priority />
-            <button style={{ opacity: showPastelButton ? 1 : 0 }} className='button-animation absolute top-1/2 left-1/2 -translate-y-2/4 -translate-x-2/4 text-xs w-[125px] h-[125px] border-2 border-white rounded-full' onClick={goToPastelDetail}>{t('showMore')}</button>
+            <button style={{ opacity: showPastelButton ? 1 : showMobileCta ? 1 : 0 }} className='button-animation absolute top-1/2 left-1/2 -translate-y-2/4 -translate-x-2/4 text-xs w-[125px] h-[125px] border-2 border-white rounded-full' onClick={goToPastelDetail}>{t('showMore')}</button>
           </div>
           <div className='white relative' onMouseEnter={() => setShowWhiteButton(true)} onMouseLeave={() => setShowWhiteButton(false)}>
             <Image src={mainWhite} alt="Freeride skiing" priority />
-            <button style={{ opacity: showWhiteButton ? 1 : 0 }} className='button-animation absolute top-1/2 left-1/2 -translate-y-2/4 -translate-x-2/4 text-xs w-[125px] h-[125px] border-2 border-white rounded-full' onClick={goToWhiteDetail}>{t('showMore')}</button>
+            <button style={{ opacity: showWhiteButton ? 1 : showMobileCta ? 1 : 0 }} className='button-animation absolute top-1/2 left-1/2 -translate-y-2/4 -translate-x-2/4 text-xs w-[125px] h-[125px] border-2 border-white rounded-full' onClick={goToWhiteDetail}>{t('showMore')}</button>
           </div>
           <div className='bnw relative' onMouseEnter={() => setShowBnwButton(true)} onMouseLeave={() => setShowBnwButton(false)}>
             <Image src={mainBnw} alt="Naj Mekinc" priority />
-            <button style={{ opacity: showBnwButton ? 1 : 0 }} className='button-animation absolute top-1/2 left-1/2 -translate-y-2/4 -translate-x-2/4 text-xs w-[125px] h-[125px] border-2 border-white rounded-full' onClick={goToBnwDetail}>{t('showMore')}</button>
+            <button style={{ opacity: showBnwButton ? 1 : showMobileCta ? 1 : 0 }} className='button-animation absolute top-1/2 left-1/2 -translate-y-2/4 -translate-x-2/4 text-xs w-[125px] h-[125px] border-2 border-white rounded-full' onClick={goToBnwDetail}>{t('showMore')}</button>
           </div>
 
         </div>
