6a67ab60663ae0009be92d7c1d26da07eca370fc
Mulsi <mulej.miha0@gmail.com>
2023-09-24 22:46:33 +0200

    Found a workaround for hover mobile events

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,7 +22,10 @@ export default function Home() {
   const { t } = useTranslation();
   const [innerWidth, setInnerWidth] = useState(0);
   const [showBlueMobileCta, setShowBlueMobileCta] = useState(false);
-  const [showMobileCta, setShowMobileCta] = useState(false);
+  const [showGreenMobileCta, setShowGreenMobileCta] = useState(false);
+  const [showPastelMobileCta, setShowPastelMobileCta] = useState(false);
+  const [showWhiteMobileCta, setShowWhiteMobileCta] = useState(false);
+  const [showBnwMobileCta, setShowBnwMobileCta] = useState(false);
 
   useEffect(() => {
     setInnerWidth(window.innerWidth);
@@ -50,9 +53,47 @@ export default function Home() {
     }
   }
 
-  const handleMobileClick = () => {
+  const handleGreenMobileTouch = () => {
     if (window.innerWidth < 640) {
-      setShowMobileCta(true);
+      setShowGreenMobileCta(true);
+    }
+  }
+  const handleGreenMobileTouchEnd = () => {
+    if (window.innerWidth < 640) {
+      setShowGreenMobileCta(false);
+    }
+  }
+
+  const handlePastelMobileTouch = () => {
+    if (window.innerWidth < 640) {
+      setShowPastelMobileCta(true);
+    }
+  }
+  const handlePastelMobileTouchEnd = () => {
+    if (window.innerWidth < 640) {
+      setShowPastelMobileCta(false);
+    }
+  }
+
+  const handleWhiteMobileTouch = () => {
+    if (window.innerWidth < 640) {
+      setShowWhiteMobileCta(true);
+    }
+  }
+  const handleWhiteMobileTouchEnd = () => {
+    if (window.innerWidth < 640) {
+      setShowWhiteMobileCta(false);
+    }
+  }
+
+  const handleBnwMobileTouch = () => {
+    if (window.innerWidth < 640) {
+      setShowBnwMobileCta(true);
+    }
+  }
+  const handleBnwMobileTouchEnd = () => {
+    if (window.innerWidth < 640) {
+      setShowBnwMobileCta(false);
     }
   }
 
@@ -86,26 +127,26 @@ export default function Home() {
       <Header></Header>
       <main className='w-11/12 mx-auto'>
         <div className={`flex flex-col gap-12 justify-center mb-8`}>
-          <div className='blue relative' onMouseEnter={() => setShowBlueButton(true)} onMouseLeave={() => setShowBlueButton(false)} onTouchStartCapture={handleBlueMobileTouch} onTouchEndCapture={handleBlueMobileTouchEnd}>
+          <div className='blue relative' onMouseEnter={() => setShowBlueButton(true)} onMouseLeave={() => setShowBlueButton(false)} onTouchStart={handleBlueMobileTouch} onTouchEnd={handleBlueMobileTouchEnd}>
             <Image src={mainBlue} alt="Mountains" priority />
             <button style={{ opacity: showBlueButton ? 1 : showBlueMobileCta ? 1 : 0 }} className={'button-animation absolute top-1/2 left-1/2 -translate-y-2/4 -translate-x-2/4 text-xs w-[125px] h-[125px] border-2 border-white rounded-full'} onClick={goToBlueDetail}>{t('showMore')}</button>
             {/* <Link href='/blue' style={{ opacity: showBlueButton ? 1 : 0 }} className='button-animation flex items-center justify-center absolute top-1/2 left-1/2 -translate-y-2/4 -translate-x-2/4 text-xs w-[125px] h-[125px] border-2 border-white rounded-full'>{t('showMore')}</Link> */}
           </div>
-          <div className='green relative' onMouseEnter={() => setShowGreenButton(true)} onMouseLeave={() => setShowGreenButton(false)}>
+          <div className='green relative' onMouseEnter={() => setShowGreenButton(true)} onMouseLeave={() => setShowGreenButton(false)} onTouchStart={handleGreenMobileTouch} onTouchEnd={handleGreenMobileTouchEnd}>
             <Image src={mainGreen} alt="Bine Žalohar" priority />
-            <button style={{ opacity: showGreenButton ? 1 : showMobileCta ? 1 : 0 }} className='button-animation absolute top-1/2 left-1/2 -translate-y-2/4 -translate-x-2/4 text-xs w-[125px] h-[125px] border-2 border-white rounded-full' onClick={goToGreenDetail}>{t('showMore')}</button>
+            <button style={{ opacity: showGreenButton ? 1 : showGreenMobileCta ? 1 : 0 }} className='button-animation absolute top-1/2 left-1/2 -translate-y-2/4 -translate-x-2/4 text-xs w-[125px] h-[125px] border-2 border-white rounded-full' onClick={goToGreenDetail}>{t('showMore')}</button>
           </div>
-          <div className='pastel relative' onMouseEnter={() => setShowPastelButton(true)} onMouseLeave={() => setShowPastelButton(false)}>
+          <div className='pastel relative' onMouseEnter={() => setShowPastelButton(true)} onMouseLeave={() => setShowPastelButton(false)} onTouchStart={handlePastelMobileTouch} onTouchEnd={handlePastelMobileTouchEnd}>
             <Image src={mainPastel} alt="Wallrider" priority />
-            <button style={{ opacity: showPastelButton ? 1 : showMobileCta ? 1 : 0 }} className='button-animation absolute top-1/2 left-1/2 -translate-y-2/4 -translate-x-2/4 text-xs w-[125px] h-[125px] border-2 border-white rounded-full' onClick={goToPastelDetail}>{t('showMore')}</button>
+            <button style={{ opacity: showPastelButton ? 1 : showPastelMobileCta ? 1 : 0 }} className='button-animation absolute top-1/2 left-1/2 -translate-y-2/4 -translate-x-2/4 text-xs w-[125px] h-[125px] border-2 border-white rounded-full' onClick={goToPastelDetail}>{t('showMore')}</button>
           </div>
-          <div className='white relative' onMouseEnter={() => setShowWhiteButton(true)} onMouseLeave={() => setShowWhiteButton(false)}>
+          <div className='white relative' onMouseEnter={() => setShowWhiteButton(true)} onMouseLeave={() => setShowWhiteButton(false)} onTouchStart={handleWhiteMobileTouch} onTouchEnd={handleWhiteMobileTouchEnd}>
             <Image src={mainWhite} alt="Freeride skiing" priority />
-            <button style={{ opacity: showWhiteButton ? 1 : showMobileCta ? 1 : 0 }} className='button-animation absolute top-1/2 left-1/2 -translate-y-2/4 -translate-x-2/4 text-xs w-[125px] h-[125px] border-2 border-white rounded-full' onClick={goToWhiteDetail}>{t('showMore')}</button>
+            <button style={{ opacity: showWhiteButton ? 1 : showWhiteMobileCta ? 1 : 0 }} className='button-animation absolute top-1/2 left-1/2 -translate-y-2/4 -translate-x-2/4 text-xs w-[125px] h-[125px] border-2 border-white rounded-full' onClick={goToWhiteDetail}>{t('showMore')}</button>
           </div>
-          <div className='bnw relative' onMouseEnter={() => setShowBnwButton(true)} onMouseLeave={() => setShowBnwButton(false)}>
+          <div className='bnw relative' onMouseEnter={() => setShowBnwButton(true)} onMouseLeave={() => setShowBnwButton(false)} onTouchStart={handleBnwMobileTouch} onTouchEnd={handleBnwMobileTouchEnd}>
             <Image src={mainBnw} alt="Naj Mekinc" priority />
-            <button style={{ opacity: showBnwButton ? 1 : showMobileCta ? 1 : 0 }} className='button-animation absolute top-1/2 left-1/2 -translate-y-2/4 -translate-x-2/4 text-xs w-[125px] h-[125px] border-2 border-white rounded-full' onClick={goToBnwDetail}>{t('showMore')}</button>
+            <button style={{ opacity: showBnwButton ? 1 : showBnwMobileCta ? 1 : 0 }} className='button-animation absolute top-1/2 left-1/2 -translate-y-2/4 -translate-x-2/4 text-xs w-[125px] h-[125px] border-2 border-white rounded-full' onClick={goToBnwDetail}>{t('showMore')}</button>
           </div>
 
         </div>
